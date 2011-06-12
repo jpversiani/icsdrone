e94c1f835fbee0dbf5ece7706dd766eb58ec4726
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2011-06-12 21:46:20 +0200

    Implemented "StartProxy".

---
+++ proxy.c
@@ -0,0 +1,26 @@
+#include "icsdrone.h"
+
+int StartProxy(){
+    struct sockaddr_in server_addr;
+    logme(LOG_DEBUG,"Starting proxy");
+    if((runData.proxyListenFd=socket(AF_INET,SOCK_STREAM,0))==-1){
+	logme(LOG_ERROR,"Could not create proxy socket.");
+	return FALSE;
+    }
+    server_addr.sin_family = AF_INET;         
+    server_addr.sin_port = htons(5000);     
+    server_addr.sin_addr.s_addr = INADDR_ANY; 
+    memset(&server_addr.sin_zero,0,sizeof(server_addr.sin_zero));
+    if (bind(runData.proxyListenFd, 
+	     (struct sockaddr *)&server_addr, 
+	     sizeof(struct sockaddr))
+	== -1) {
+	logme(LOG_ERROR,"Unable to bind proxy socket.");
+	return FALSE;
+    }
+    if (listen(runData.proxyListenFd, 1) == -1) {
+	logme(LOG_ERROR,"Unable to bind proxy socket.");
+	return FALSE;
+    }
+    return TRUE;
+}
