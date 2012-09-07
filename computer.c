7c7590b3ae16e294ca1c2236787b05d0bda3bba0
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2012-09-07 06:23:36 +0200

    Ignore comments in computer output.

--- computer.c
+++ computer.c
@@ -439,7 +439,9 @@ void ProcessComputerLine(char *line, char *queue)
       logme(LOG_WARNING, 
 	    "Move received from engine but we're not playing anymore!"); 
     }
-  }else if(!strncasecmp(line,"Illegal move",12)){
+  }else if(line[0]=='#'){
+    /* Comment! Ignore. */
+  }else if(!strncasecmp(line,"Illegal move",12) && runData.inGame){
       	logme(LOG_DEBUG,"Something bad happened. Bailing out.");
 	SendToIcs("resign\n");
   } else if (!strncmp(line, "pong ", 5)) {
