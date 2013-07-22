4a202359ade22e070c8cc03fa07ac7ad70773265
Michel Van den Bergh <michel.vandenbergh@uhasselt.be>
2013-07-22 12:03:20 +0200

    Do not allow changing engineKnowsSAN at runtime.

--- argparser.c
+++ argparser.c
@@ -95,7 +95,7 @@ ArgList argList = {
     {"-feedbackCommand",      ArgString, &appData.feedbackCommand   , 1 },
     {"-engineQuiet",          ArgBool,   &appData.engineQuiet       , 1 },
     {"-variants",             ArgString, &appData.variants          , 0 },
-    {"-engineKnowsSAN",       ArgBool,   &appData.engineKnowsSAN    , 1 },
+    {"-engineKnowsSAN",       ArgBool,   &appData.engineKnowsSAN    , 0 },
     {"-tourneyFilter",        ArgString, &appData.tourneyFilter     , 1 },
     {"-matchFilter",          ArgString, &appData.matchFilter       , 1 },
     {NULL,                    ArgNull,   NULL                       , 0 } 
